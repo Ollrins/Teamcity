e490c56edf777657a28c87739effbc238c213058
Ollrins <ollrins@gmail.com>
2026-05-13 22:26:57 +0300

    Update WelcomerTest.java

--- src/main/java/plaindoll/WelcomerTest.java
+++ src/main/java/plaindoll/WelcomerTest.java
@@ -1,15 +1,1 @@
-package plaindoll;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import org.junit.Test;
-
-public class WelcomerTest {
-    
-    @Test
-    public void testGetHunterReplyContainsHunter() {
-        Welcomer welcomer = new Welcomer();
-        String reply = welcomer.getHunterReply();
-        assertThat(reply, containsString("hunter"));
-    }
-}
