f58fd0679f73183e40956e59d7e7050e6937cc83
Ollrins <ollrins@gmail.com>
2026-05-13 22:40:49 +0300

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,10 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+    public void testGetHunterReplyContainsHunter() {
+    Welcomer welcomer = new Welcomer();
+    String reply = welcomer.getHunterReply();
+    assertThat(reply, containsString("hunter"));
+    }
 }
